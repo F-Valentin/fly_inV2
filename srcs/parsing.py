bd5d1cd308f40819b29d1c7a44b597eee9d51869
Valentin Fechter <vafechte@k1r1p5.42mulhouse.fr>
2026-05-06 18:41:57 +0200

    feat: add Parser class to encapsulate the parsing logic

--- srcs/parsing.py
+++ srcs/parsing.py
@@ -1,41 +1,47 @@
 class ParsingError(Exception):
     pass
 
-
-def _get_nb_drones(line: str) -> int:
-    try:
-        nb_drones: int = int(line.split(":")[1])
-
-        if nb_drones <= 0:
-            raise ParsingError("nb_drones must be a positive integer.")
-
-    except ValueError as e:
-        raise e
-
-    return nb_drones
+class Parser:
+    def __init__(self, file_lines: list[str]) -> None:
+            self.file_lines = file_lines
+            self.zones: dict[str, Zone] = {}
+            self.connections: list[Connection] = []
 
 
-def parse(file_lines: list[str]):
-    data: dict[str, int | list[Zone] | list[Connection]] = {}
-    next_line_index: int = 0
+    def _get_nb_drones(self, line: str) -> int:
+        try:
+            nb_drones: int = int(line.split(":")[1])
 
-    for (i, line) in enumerate(file_lines):
-        if not line.strip() or line.startswith("#"):
-            continue
+            if nb_drones <= 0:
+                raise ParsingError("nb_drones must be a positive integer.")
 
-        if line.startswith("nb_drones: "):
-            try:
-                nb_drones: int = _get_nb_drones(line)
-            except (ValueError, ParsingError) as e:
-                raise e
+        except ValueError as e:
+            raise e
 
-            data["nb_drones"] = nb_drones
-            next_line_index = i + 1
-            break
+        return nb_drones
 
-        raise ParsingError("The first line must contain nb_drones: X "
-                           "(where X is a positive integer)")
-    
-    for line in file_lines[next_line_index:]:
-        if not line.strip() or line.startswith("#"):
-            continue
+
+    def parse(self):
+        data: dict[str, int | list[Zone] | list[Connection]] = {}
+        next_line_index: int = 0
+
+        for (i, line) in enumerate(self.file_lines):
+            if not line.strip() or line.startswith("#"):
+                continue
+
+            if line.startswith("nb_drones: "):
+                try:
+                    nb_drones: int = self._get_nb_drones(line)
+                except (ValueError, ParsingError) as e:
+                    raise e
+
+                data["nb_drones"] = nb_drones
+                next_line_index = i + 1
+                break
+
+            raise ParsingError("The first line must contain nb_drones: X "
+                            "(where X is a positive integer)")
+        
+        for line in self.file_lines[next_line_index:]:
+            if not line.strip() or line.startswith("#"):
+                continue
